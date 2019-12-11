f0c890f52ae31e26cebc4cae1abc728533edcef1
ltf0501 <0110420@stu.nknush.kh.edu.tw>
2019-12-12 00:31:31 +0800

    faster NTT template

--- Codebook/math/NTT.cpp
+++ Codebook/math/NTT.cpp
@@ -1,98 +1,66 @@
-//g=3,3,3
-//mod=998244353,1004535809,469762049
-const int g=3;
-const int mod=998244353;
-ll pw(ll a,int n,ll mod)
-{
-	ll res=1;
-	while(n)
-	{
-		if(n&1)res=res*a%mod;
-		a=a*a%mod,n>>=1;
-	}
-	return res;
-}
-ll INV(ll a,ll mod){return pw(a,mod-2,mod);}
-ll mul(ll a,ll b,int mod)//O(1) 
-{
-	a%=mod,b%=mod;
-	return (a*b-(ll)((long double)a*b/mod)*mod+mod)%mod;
-}
-int find_root(int p)//find primitive root
-{
-	int tmp=x-1;
-	vector<int> v;
-	for(int i=2;i<=(int)sqrt(tmp);i++)
-	{
-		if(tmp%i==0)v.push_back(i);
-		while(tmp%i==0)tmp/=i;
-	}
-	for(int g=2;g<x;g++)
-	{
-		bool flag=1;
-		for(int t : v)
-			if(pw(g,(x-1)/t)==1){flag=0;break ;}
-		if(flag)return g;
-	}
-}
-struct NTT
-{
-	int n,rev[maxn];
-	int g,mod;
-	ll omega[maxn],iomega[maxn];
-	void init(int n,int g,int mod)
-	{
-		this->n=n;
-		this->g=g;
-		this->mod=mod;
-		int x=pw(g,(mod-1)/n);
-		omega[0]=1;
-		for(int i=1;i<n;i++)omega[i]=omega[i-1]*x%mod;
-		iomega[n-1]=INV(omega[n-1]);
-		for(int i=n-2;i>=0;i--)iomega[i]=iomega[i+1]*x%mod;
-		int k=log2(n);
-		for(int i=0;i<n;i++)
-		{
-			int t=0;
-			for(int j=0;j<k;j++)if(i&(1<<j))t|=(1<<(k-j-1));
+/* p=a*2^k+1
+   p                     a      k      root
+   998244353             119    23     3
+   2013265921            15     27     31
+   2061584302081         15     37     7
+   2748779069441         5      39     3
+   1945555039024054273	 27     56     5 */
+struct NTT {
+	int n, invn, rev[kN];
+	int g;
+	int omega[kN], iomega[kN];
+	void init(int n, int g) {
+		this->n = n, this->g = g;
+		invn = Inv(n);
+		int k = log2(n);
+		int x = pw(g, (kMod - 1) / n);
+		omega[0] = 1;
+		for(int i = 1; i < n; i++) omega[i] = 1LL * omega[i - 1] * x % kMod;
+		iomega[n - 1] = Inv(omega[n - 1]);
+		for(int i = n - 2; i >= 0; i--) iomega[i] = 1LL * iomega[i + 1] * x % kMod;
+
+		for(int i = 0; i < n; i++) {
+			int t = 0;
+			for(int j = 0; j < k; j++) 
+				if(i & (1 << j)) t |= (1 << (k - j - 1));
+			rev[i] = t;
 		}
 	}
-	void transform(vector<ll>& a,ll* omega)
-	{
-		for(int i=0;i<n;i++)if(i<rev[i])swap(a[i],a[rev[i]]);
-		for(int len=2;len<=n;len<<=1)
-		{
-			int mid=len>>1;
-			for(int j=0;j<len;j+=len)for(int i=0;i<mid;i++)
-			{
-				ll t=omega[n/len*i]*a[j+mid+i]%mod;
-				a[j+mid+i]=(a[j+i]-t+mod)%mod;
-				a[j+i]=(a[j+i]+t)%mod;
+	void transform(vector<int> &a, int *omega) {
+		for(int i = 0; i < n; i++) 
+			if(i < rev[i]) swap(a[i], a[rev[i]]);
+
+		for(int len = 2; len <= n; len <<= 1) {
+			int mid = len >> 1;
+			int r = n / len;
+			for(int j = 0; j < n; j += len) {
+				for(int i = 0; i < mid; i++) {
+					int t = 1LL * omega[r * i] * a[j + mid + i] % kMod;
+					a[j + mid + i] = a[j + i] - t;
+					if(a[j + mid + i] < 0) a[j + mid + i] += kMod;
+					a[j + i] += t;
+					if(a[j + i] >= kMod) a[j + i] -= kMod;
+				}
 			}
 		}
 	}
-	void dft(vector<ll>& a)
-	{
-		transform(a,omega);
+	void dft(vector<int> &a) {
+		transform(a, omega);
 	}
-	void idft(vector<ll>& a)
-	{
-		transform(a,iomega);
-		ll x=INV(n,mod);
-		for(inti =0;i<n;i++)a[i]=a[i]*x%mod;
-	}
+	void idft(vector<int> &a) {
+		transform(a, iomega);
+		for(int i = 0; i < n; i++) a[i] = 1LL * a[i] * invn % kMod;
+	} 
 };
-vector<ll> mul(vector<ll> a,vector<ll> b,int mod)
-{
-	int N=1;
-	int n1=(int)a.size(),n2=(int)b.size();
-	while(N<n1+n2)N<<=1;
-	NTT solver;
-	solver.init(N);
-	solver.dft(a),solver.dft(b);
-	for(int i=0;i<n;i++)a[i]=a[i]*b[i]%mod;
-	idft(a);
-	vector<ll> ans(n);
-	for(int i=0;i<n;i++)ans[i]=a[i];
-	return ans;
+vector<int> mul(vector<int> a, vector<int> b) {
+	int n = 1;
+	int n1 = int(a.size()), n2 = int(b.size());
+	while(n < n1 + n2 - 1) n <<= 1;
+	a.resize(n), b.resize(n);
+	solver.init(n, 3); // 3 is primitive root for 998244353
+	solver.dft(a), solver.dft(b);
+	for(int i = 0; i < n; i++) a[i] = 1LL * a[i] * b[i] % kMod;
+	solver.idft(a);
+	while(!a.empty() && a.back() == 0) a.pop_back();
+	return a;
 }
